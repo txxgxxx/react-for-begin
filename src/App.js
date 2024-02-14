0ac86c7dfeed67892036eecdc4ee5cd0381e6579
txxgxxx <taegun1020@naver.com>
2024-02-14 22:32:20 +0900

    7.3 movie app part one

--- src/App.js
+++ src/App.js
@@ -2,59 +2,41 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [selectedCoin, setSelectedCoin] = useState(null);
-  const [money, setMoney] = useState(0);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    ).json();
+
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
-  const onInputChange = (event) => {
-    setMoney(event.target.value);
-  };
-  const onChange = (event) => {
-    const values = event.target.value.split(",");
-    const coin = {
-      price: values[0],
-      symbol: values[1],
-    };
-    setSelectedCoin(coin);
-  };
   return (
     <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      <input
-        onChange={onInputChange}
-        value={money}
-        type="number"
-        placeholder="Your money"
-      ></input>
-      {selectedCoin ? (
-        <h3>
-          You have ${money}, you can buy{" "}
-          {Math.floor(money / selectedCoin.price)}
-          {selectedCoin.symbol}{" "}
-        </h3>
-      ) : null}
-      <hr />
       {loading ? (
-        <strong>Loading...</strong>
+        <h1>Loading...</h1>
       ) : (
-        <select onChange={onChange}>
-          <option>Choose coin</option>
-          {coins.map((coin) => (
-            <option
-              key={coin.id}
-              value={`${coin.quotes.USD.price}, ${coin.symbol}`}
-            >
-              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
-            </option>
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              {movie.hasOwnProperty("genres") ? (
+                <ul>
+                  {movie.genres.map((genre) => (
+                    <li key={genre}>{genre}</li>
+                  ))}
+                </ul>
+              ) : null}
+            </div>
           ))}
-        </select>
+        </div>
       )}
     </div>
   );
