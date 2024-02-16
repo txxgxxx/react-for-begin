95217290493c43d48cfb7d31471425aea15f5316
txxgxxx <taegun1020@naver.com>
2024-02-16 21:02:15 +0900

    7.7  Publishing

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,18 +1,22 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-function Movie({ id, coverImg, title, summary, genres }) {
+import styles from "./Movie.module.css";
+function Movie({ id, coverImg, title, year, summary, genres }) {
   return (
-    <div>
-      <img src={coverImg} alt={title} />
-      <h2>
-        <Link to={`/movie/${id}`}>{title}</Link>
-      </h2>
-      <p>{summary}</p>
-      <ul>
-        {genres.map((genre) => (
-          <li key={genre}>{genre}</li>
-        ))}
-      </ul>
+    <div className={styles.movie}>
+      <img src={coverImg} alt={title} className={styles.movie_img} />
+      <div>
+        <h2 className={styles.movie_title}>
+          <Link to={`/movie/${id}`}>{title}</Link>
+        </h2>
+        <h3 className={styles.movie_year}>{year}</h3>
+        <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
+        <ul className={styles.movie_genres}>
+          {genres.map((genre) => (
+            <li key={genre}>{genre}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import styles from "../styles/Detail.module.css";
+import styles from "./Detail.module.css";
 function Detail() {
   const { id } = useParams();
   const [loading, setLoading] = useState(true);
@@ -20,16 +20,29 @@ function Detail() {
       {loading ? (
         <h1>loading...</h1>
       ) : (
-        <div>
-          <img src={movieDetail.large_cover_image} alt={movieDetail.title} />
-          <h2>{movieDetail.title}</h2>
-          <div>
-            <p className={`${styles.des}`}>{movieDetail.description_full}</p>
-            <ul>
-              {movieDetail.genres.map((genre) => (
-                <li key={genre}>{genre}</li>
-              ))}
-            </ul>
+        <div className={styles.container}>
+          <div className={styles.detail}>
+            <div>
+              <img
+                src={movieDetail.large_cover_image}
+                alt={movieDetail.title}
+                className={styles.detail__poster}
+              />
+            </div>
+            <div className={styles.detail__info}>
+              <div>
+                <h2>{movieDetail.title}</h2>
+                <ul>
+                  <li>{movieDetail.year}</li>
+                  {movieDetail.genres.map((genre) => (
+                    <li key={genre}>{genre}</li>
+                  ))}
+                </ul>
+              </div>
+              <div>
+                <p>{movieDetail.description_full}</p>
+              </div>
+            </div>
           </div>
         </div>
       )}
